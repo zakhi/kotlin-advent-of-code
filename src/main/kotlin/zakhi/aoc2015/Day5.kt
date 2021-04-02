12f231af8901984eb210e26c1219d4b2772a472b
Zakhi Verech <zakhi@omnisol.co.il>
2021-04-02 23:55:05 +0300

    2015 Day 5

---
+++ src/main/kotlin/zakhi/aoc2015/Day5.kt
@@ -0,0 +1,24 @@
+package zakhi.aoc2015
+
+import zakhi.linesOf
+
+
+fun main() {
+    val strings = linesOf("aoc2015/day5").toList()
+
+    val niceStrings = strings.count { it.isNice }
+    println("The number of nice strings is $niceStrings")
+
+    val reallyNiceStrings = strings.count { it.isReallyNice }
+    println("The number of really nice strings is $reallyNiceStrings")
+}
+
+
+private val String.isNice: Boolean get() =
+    contains(Regex("([aeiou].*){3}")) &&
+    contains(Regex("""(\w)\1""")) &&
+    !contains(Regex("ab|cd|pq|xy"))
+
+private val String.isReallyNice: Boolean get() =
+    contains(Regex("""(\w\w).*\1""")) &&
+    contains(Regex("""(\w).\1"""))
