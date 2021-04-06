c141f1519d956000e1cd09c9a4ede4f76d4d43e8
Zakhi Verech <zakhi@omnisol.co.il>
2021-04-06 22:24:00 +0300

    2015 Day 10

---
+++ src/main/kotlin/zakhi/aoc2015/Day10.kt
@@ -0,0 +1,22 @@
+package zakhi.aoc2015
+
+import zakhi.entireTextOf
+
+
+fun main() {
+    val initialSequence = entireTextOf("aoc2015/day10")
+
+    val resultAfter40 = generateSequences(initialSequence, times = 40)
+    println("The length of the result after 40 times is ${resultAfter40.length}")
+
+    val resultAfter50 = generateSequences(resultAfter40, times = 10)
+    println("The length of the result after 50 times is ${resultAfter50.length}")
+}
+
+
+private fun generateSequences(initialSequence: String, times: Int) =
+    (1..times).fold(initialSequence) { currentSequence, _ ->
+        Regex("""(\d)\1*""").findAll(currentSequence).joinToString(separator = "") {
+            "${it.value.length}${it.groupValues[1]}"
+        }
+    }
