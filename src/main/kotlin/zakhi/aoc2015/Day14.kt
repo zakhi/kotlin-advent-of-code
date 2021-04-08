78fb3e27492a82e0874a88dabd14d7557239dd3a
Zakhi Verech <zakhi@omnisol.co.il>
2021-04-08 12:07:11 +0300

    2015 Day 14

---
+++ src/main/kotlin/zakhi/aoc2015/Day14.kt
@@ -0,0 +1,42 @@
+package zakhi.aoc2015
+
+import zakhi.matchEachLineOf
+
+
+fun main() {
+    val reindeer = matchEachLineOf("aoc2015/day14", Regex("""\w+ can fly (\d+) km/s for (\d+) seconds, but then must rest for (\d+) seconds.""")) {
+        (speed, flyTime, restTime) -> FlyingReindeer(speed.toInt(), flyTime.toInt(), restTime.toInt())
+    }
+
+    val raceTime = 2503
+
+    val winningDistance = reindeer.maxOf { it.distanceCoveredIn(raceTime) }
+    println("The distance covered by the winner is $winningDistance")
+
+    (1..raceTime).forEach { timePassed -> reindeer.maxByOrNull { it.distanceCoveredIn(timePassed) }?.awardPoint() }
+
+    val winningPoints = reindeer.maxOf { it.points }
+    println("The winner has $winningPoints points")
+}
+
+
+private class FlyingReindeer(
+    private val speed: Int,
+    private val flyTime: Int,
+    private val restTime: Int,
+) {
+    var points: Int = 0
+        private set
+
+    fun distanceCoveredIn(seconds: Int): Int {
+        val fullRoundsDistance = seconds / (flyTime + restTime) * speed * flyTime
+        val remainder = seconds % (flyTime + restTime)
+        val remainderDistance = minOf(remainder, flyTime) * speed
+
+        return fullRoundsDistance + remainderDistance
+    }
+
+    fun awardPoint() {
+        points += 1
+    }
+}
