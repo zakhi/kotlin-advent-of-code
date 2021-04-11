b4dd5719fe976822c0408773e8952c33cff7ea50
Zakhi Verech <zakhi@omnisol.co.il>
2021-04-11 16:23:48 +0300

    2015 Day 21

--- src/main/kotlin/zakhi/Collections.kt
+++ src/main/kotlin/zakhi/Collections.kt
@@ -5,3 +5,10 @@ fun <E> List<E>.permutations(): List<List<E>> {
     if (size == 1) return listOf(this)
     return flatMap { element -> (this - element).permutations().map { it + element } }
 }
+
+
+fun <E> List<E>.pairs(): List<Pair<E, E>> {
+    return flatMapIndexed { index, element -> slice(index + 1..lastIndex).map { secondElement -> element to secondElement } }
+}
+
+fun <T1, T2> Pair<T1, T2>.flip(): Pair<T2, T1> = second to first
---
+++ src/main/kotlin/zakhi/aoc2015/Day21.kt
@@ -0,0 +1,121 @@
+package zakhi.aoc2015
+
+import zakhi.flip
+import zakhi.linesOf
+import zakhi.pairs
+
+
+fun main() {
+    val winningEquipment = equipmentSets().sortedBy { equipment -> equipment.sumOf { it.cost } }.first { equipment ->
+        val player = createPlayer(equipment)
+        val boss = createBoss()
+
+        winnerOfFightBetween(player, boss) == player
+    }
+
+    println("The least amount of money spent to win is ${winningEquipment.sumOf { it.cost }}")
+
+    val losingEquipment = equipmentSets().sortedByDescending { equipment -> equipment.sumOf { it.cost } }.first { equipment ->
+        val player = createPlayer(equipment)
+        val boss = createBoss()
+
+        winnerOfFightBetween(player, boss) == boss
+    }
+
+    println("The most amount of money spent and lose is ${losingEquipment.sumOf { it.cost }}")
+}
+
+
+private val input = linesOf("aoc2015/day21").toList()
+
+private val weapons = listOf(
+    Equipment(cost = 8, damage = 4),
+    Equipment(cost = 10, damage = 5),
+    Equipment(cost = 25, damage = 6),
+    Equipment(cost = 40, damage = 7),
+    Equipment(cost = 74, damage = 8)
+)
+
+private val armors = listOf(
+    Equipment(cost = 13, armor = 1),
+    Equipment(cost = 31, armor = 2),
+    Equipment(cost = 53, armor = 3),
+    Equipment(cost = 75, armor = 4),
+    Equipment(cost = 102, armor = 5)
+)
+
+private val rings = listOf(
+    Equipment(cost = 25, damage = 1),
+    Equipment(cost = 50, damage = 2),
+    Equipment(cost = 100, damage = 3),
+    Equipment(cost = 20, armor = 1),
+    Equipment(cost = 40, armor = 2),
+    Equipment(cost = 80, armor = 3)
+)
+
+private fun equipmentSets(): Sequence<List<Equipment>> = sequence {
+    weapons.forEach { weapon ->
+        yield(listOf(weapon))
+
+        armors.forEach { armor ->
+            yield(listOf(weapon, armor))
+
+            rings.forEach { ring ->
+                yield(listOf(weapon, ring))
+                yield(listOf(weapon, armor, ring))
+            }
+
+            rings.pairs().forEach { (firstRing, secondRing) ->
+                yield(listOf(weapon, firstRing, secondRing))
+                yield(listOf(weapon, armor, firstRing, secondRing))
+            }
+        }
+    }
+}
+
+private fun createBoss() = Fighter(
+    initialHitPoints = findProperty(Regex("""Hit Points: (\d+)""")),
+    damage = findProperty(Regex("""Damage: (\d+)""")),
+    armor = findProperty(Regex("""Armor: (\d+)"""))
+)
+
+private fun createPlayer(equipments: List<Equipment>) = Fighter(
+    initialHitPoints = 100,
+    damage = equipments.sumOf { it.damage },
+    armor = equipments.sumOf { it.armor }
+)
+
+private fun findProperty(regex: Regex): Int =
+    input.mapNotNull { regex.matchEntire(it)?.groupValues?.get(1)?.toInt() }.first()
+
+private fun winnerOfFightBetween(first: Fighter, second: Fighter): Fighter {
+    var fighters = first to second
+
+    while (true) {
+        fighters.first.hit(fighters.second)
+        if (fighters.second.isDead) return fighters.first
+
+        fighters = fighters.flip()
+    }
+}
+
+private data class Equipment(
+    val cost: Int,
+    val damage: Int = 0,
+    val armor: Int = 0
+)
+
+class Fighter(
+    initialHitPoints: Int,
+    private val damage: Int,
+    private val armor: Int
+) {
+
+    val isDead: Boolean get() = hitPoints <= 0
+
+    fun hit(other: Fighter) {
+        other.hitPoints -= maxOf(1, damage - other.armor)
+    }
+
+    private var hitPoints = initialHitPoints
+}
