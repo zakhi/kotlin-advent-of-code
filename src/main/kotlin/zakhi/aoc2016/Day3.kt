3217ae683c1b026d869c9c3ad0102f3de54ae7e1
Zakhi Verech <zakhi@omnisol.co.il>
2021-04-26 10:59:22 +0300

    2016 Day 3

---
+++ src/main/kotlin/zakhi/aoc2016/Day3.kt
@@ -0,0 +1,30 @@
+package zakhi.aoc2016
+
+import zakhi.matchEachLineOf
+
+
+fun main() {
+    val triples = matchEachLineOf("aoc2016/day3", Regex("""\s*(\d+)\s+(\d+)\s+(\d+)""")) { (a, b, c) ->
+        Triple(a.toInt(), b.toInt(), c.toInt())
+    }
+
+    val triangles = triples.count { it.isPossibleTriangle }
+    println("There are $triangles possible triangles")
+
+    val columnTriples = triples.chunked(3).flatMap { (a, b, c) ->
+        listOf(
+            Triple(a.first, b.first, c.first),
+            Triple(a.second, b.second, c.second),
+            Triple(a.third, b.third, c.third),
+        )
+    }
+
+    val columnTriangles = columnTriples.count { it.isPossibleTriangle }
+    println("There are $columnTriangles possible column triangles")
+
+}
+
+
+private val Triple<Int, Int, Int>.isPossibleTriangle: Boolean get() = with(toList()) {
+    return all { side -> side * 2 < sum() }
+}
