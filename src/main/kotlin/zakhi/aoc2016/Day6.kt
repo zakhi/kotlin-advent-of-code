0dc49f554cde9c8cba8228a1a7228d5ac26df2fd
Zakhi Verech <zakhi@omnisol.co.il>
2021-04-27 22:05:26 +0300

    2016 Day 6

---
+++ src/main/kotlin/zakhi/aoc2016/Day6.kt
@@ -0,0 +1,21 @@
+package zakhi.aoc2016
+
+import zakhi.join
+import zakhi.linesOf
+
+
+fun main() {
+    val messages = linesOf("aoc2016/day6").toList()
+    val letterFrequencies = messages.first().indices.map { index ->
+        messages.map { it[index] }.groupingBy { it }.eachCount()
+    }
+
+    val sentMessage = letterFrequencies.join { it.mostCommonChar() }
+    println("The message is $sentMessage")
+
+    val actualSentMessage = letterFrequencies.join { it.leastCommonChar() }
+    println("The actual message is $actualSentMessage")
+}
+
+private fun Map<Char, Int>.mostCommonChar() = maxByOrNull { it.value }?.key.toString()
+private fun Map<Char, Int>.leastCommonChar() = minByOrNull { it.value }?.key.toString()
