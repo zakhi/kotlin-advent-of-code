aed1f47266667b57ca14eaadcd72b696db7a5a6f
Zakhi Verech <zakhi@omnisol.co.il>
2021-06-16 11:29:28 +0300

    2016 Day 17

---
+++ src/main/kotlin/zakhi/aoc2016/Day17.kt
@@ -0,0 +1,45 @@
+package zakhi.aoc2016
+
+import zakhi.Point
+import zakhi.input.entireTextOf
+import zakhi.points.plus
+import zakhi.points.x
+import zakhi.points.y
+import zakhi.strings.md5Hash
+
+
+fun main() {
+    val shortestPath = findShortestPath(from = 1 to 1)
+    println("The shortest path is $shortestPath")
+
+    val longestPath = findLongestPath(from = 1 to 1)
+    println("The longest path's length is ${longestPath?.length}")
+}
+
+
+private val passcode = entireTextOf("aoc2016/day17").trim()
+
+private fun findShortestPath(from: Point) = findBestPath(from, "", shortest = true)
+private fun findLongestPath(from: Point) = findBestPath(from, "", shortest = false)
+
+private fun findBestPath(from: Point, currentPath: String = "", shortest: Boolean): String? {
+    if (from.x !in 1..4 || from.y !in 1..4) return null
+    if (from == 4 to 4) return currentPath
+
+    val openDoors = "$passcode$currentPath".md5Hash().take(4).mapIndexedNotNull { index, code ->
+        if (code in 'b'..'f') Direction.values()[index] else null
+    }
+
+    val paths = openDoors.mapNotNull { direction ->
+        findBestPath(from = from + direction.offset, currentPath = currentPath + direction.name, shortest)
+    }
+
+    return if (shortest) paths.minByOrNull { it.length } else paths.maxByOrNull { it.length }
+}
+
+private enum class Direction(val offset: Point) {
+    U(0 to -1),
+    D(0 to 1),
+    L(-1 to 0),
+    R(1 to 0)
+}
