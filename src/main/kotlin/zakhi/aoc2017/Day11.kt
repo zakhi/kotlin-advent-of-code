da4aa0fa2205012ff899690aa08ad555005907de
Zakhi Verech <zakhi@omnisol.co.il>
2021-09-15 19:32:57 +0300

    2017 Day 11

---
+++ src/main/kotlin/zakhi/aoc2017/Day11.kt
@@ -0,0 +1,36 @@
+package zakhi.aoc2017
+
+import zakhi.helpers.*
+
+
+fun main() {
+    val instructions = entireTextOf("aoc2017/day11").trim().split(",")
+    val track = HexGridTrack()
+    instructions.forEach { track.go(it) }
+
+    println("The number of steps from the start is ${track.distanceFromStart}")
+    println("The furthest distance is ${track.furthestDistance}")
+}
+
+class HexGridTrack {
+    private var position = 0 to 0
+
+    var furthestDistance = 0
+        private set
+
+    val distanceFromStart: Int get() = maxOf(position.x, position.y / 2)
+
+    fun go(direction: String) {
+        position += when (direction) {
+            "n" -> 0 to 2
+            "s" -> 0 to -2
+            "ne" -> 1 to 1
+            "se" -> 1 to -1
+            "nw" -> -1 to 1
+            "sw" -> -1 to -1
+            else -> throw Exception("Invalid direction $direction")
+        }
+
+        furthestDistance = maxOf(furthestDistance, distanceFromStart)
+    }
+}
