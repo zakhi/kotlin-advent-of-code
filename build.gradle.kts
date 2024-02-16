afce347d0b43ebe4c266ab63aeec45911bcc875b
Zakhi Verech <zakhi.verech@cajarobotics.com>
2024-02-16 14:37:24 +0200

    Update Gradle and Kotlin versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.8.0"
-    kotlin("plugin.serialization") version "1.8.0"
+    kotlin("jvm") version "1.9.22"
+    kotlin("plugin.serialization") version "1.9.22"
 }
 
 group = "zakhi"
